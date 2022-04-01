312bf51efa355a1ac606292d226b0cf10785dedc
ChrisNieves <nievescs20@gmail.com>
2022-03-31 22:00:20 -0400

    created Footer component and Footer CSS

---
+++ src/components/About/Footer/Footer.js
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <a
+      href="https://github.com/Nievescs20/Portfolio"
+      className="link footer-link"
+    >
+      Created By Christopher Nieves
+    </a>
+  </footer>
+);
+
+export default Footer;
