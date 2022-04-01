92a4f4b2da8c24f54a2382b7db5977d3ce679e08
ChrisNieves <nievescs20@gmail.com>
2022-03-31 22:01:57 -0400

    created footer folder outside of component so I moved it inside of components folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import About from "./components/About/About";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div>
       <About />
+      <Footer />
     </div>
   );
 }
