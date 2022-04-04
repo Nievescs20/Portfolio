33fd649ceffa6daead39b9b7d748dd8a6757dce4
ChrisNieves <nievescs20@gmail.com>
2022-04-04 15:31:28 -0400

    removing all dark mode code. hoping to deploy

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,21 +1,15 @@
-import { useContext, useState } from "react";
-// import Brightness2Icon from "@material-ui/icons/Brightness2";
-// import WbSunnyRoundedIcon from "@material-ui/icons/WbSunnyRounded";
-// import MenuIcon from "@material-ui/icons/Menu";
-// import CloseIcon from "@material-ui/icons/Close";
-// import { ThemeContext } from "../../contexts/theme";
+import { useState } from "react";
 import { projects, skills } from "../../info";
 import "./Navbar.css";
 
 const Navbar = () => {
-  // const [{ themeName, toggleTheme }] = useContext(ThemeContext);
   const [showNavList, setShowNavList] = useState(false);
 
   const toggleNavList = () => setShowNavList(!showNavList);
 
   return (
     <nav className="center nav">
-      <ul style={{ display: showNavList ? "flex" : null }} className="nav-list">
+      <ul className="nav-list">
         {projects.length ? (
           <li className="nav-list-item">
             <a
@@ -36,24 +30,6 @@ const Navbar = () => {
           </li>
         ) : null}
       </ul>
-
-      {/* <button
-        type="button"
-        onClick={toggleTheme}
-        className="btn btn-icon nav-theme"
-        aria-label="toggle theme"
-      >
-        {themeName === "dark" ? <WbSunnyRoundedIcon /> : <Brightness2Icon />}
-      </button>
-
-      <button
-        type="button"
-        onClick={toggleNavList}
-        className="btn btn-icon nav-hamburger"
-        aria-label="toggle navigation"
-      >
-        {showNavList ? <CloseIcon /> : <MenuIcon />}
-      </button> */}
     </nav>
   );
 };
